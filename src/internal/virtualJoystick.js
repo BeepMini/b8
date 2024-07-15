b1f81fdbbc9d6623fc1c8cf065e7be8737ab3080
Ben Gillbanks <binarymoon@gmail.com>
2024-07-15 11:19:03 +0100

    reduce whitespace

--- src/internal/virtualJoystick.js
+++ src/internal/virtualJoystick.js
@@ -19,10 +19,10 @@
 	 * @type {string}
 	 */
 	const LEFT_VJOY_HTML = `
-		<div id='vjoy-button-up' class='vjoy-button'></div>
-		<div id='vjoy-button-down' class='vjoy-button'></div>
-		<div id='vjoy-button-left' class='vjoy-button'></div>
-		<div id='vjoy-button-right' class='vjoy-button'></div>
+<div id='vjoy-button-up' class='vjoy-button'></div>
+<div id='vjoy-button-down' class='vjoy-button'></div>
+<div id='vjoy-button-left' class='vjoy-button'></div>
+<div id='vjoy-button-right' class='vjoy-button'></div>
 	`;
 
 
@@ -33,9 +33,9 @@
 	 * @type {string}
 	 */
 	const RIGHT_VJOY_HTML = `
-		<div id='vjoy-button-pri' class='vjoy-button'>A</div>
-		<div id='vjoy-button-sec' class='vjoy-button'>B</div>
-		<div id='vjoy-button-ter' class='vjoy-button'>=</div>
+<div id='vjoy-button-pri' class='vjoy-button'>A</div>
+<div id='vjoy-button-sec' class='vjoy-button'>B</div>
+<div id='vjoy-button-ter' class='vjoy-button'>=</div>
 	`;
 
 
@@ -45,128 +45,128 @@
 	 * @type {string}
 	 */
 	const VJOY_CSS = `
-		* {
-			user-select: none;
-			-webkit-user-select: none;
-			-webkit-touch-callout: none;
-		}
+* {
+	user-select: none;
+	-webkit-user-select: none;
+	-webkit-touch-callout: none;
+}
 
-		#vjoy-scrim {
-			position: fixed;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			top: 0;
-			pointer-events: all;
-		}
+#vjoy-scrim {
+	position: fixed;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	top: 0;
+	pointer-events: all;
+}
 
-		#vjoy-container-left {
-			box-sizing: border-box;
-			position: fixed;
-			bottom: 16px;
-			left: 16px;
-			width: 40vmin;
-			height: 40vmin;
-			user-select: none;
-			touch-callout: none;
-			-webkit-user-select: none;
-			-webkit-touch-callout: none;
-		}
+#vjoy-container-left {
+	box-sizing: border-box;
+	position: fixed;
+	bottom: 16px;
+	left: 16px;
+	width: 40vmin;
+	height: 40vmin;
+	user-select: none;
+	touch-callout: none;
+	-webkit-user-select: none;
+	-webkit-touch-callout: none;
+}
 
-		#vjoy-container-right {
-			box-sizing: border-box;
-			position: fixed;
-			bottom: 16px;
-			right: 16px;
-			width: 40vmin;
-			height: 40vmin;
-			user-select: none;
-			touch-callout: none;
-			-webkit-user-select: none;
-			-webkit-touch-callout: none;
-		}
+#vjoy-container-right {
+	box-sizing: border-box;
+	position: fixed;
+	bottom: 16px;
+	right: 16px;
+	width: 40vmin;
+	height: 40vmin;
+	user-select: none;
+	touch-callout: none;
+	-webkit-user-select: none;
+	-webkit-touch-callout: none;
+}
 
-		.vjoy-button {
-			display: flex;
-			flex-direction: row;
-			align-items: center;
-			justify-content: center;
-			background: #444;
-			border: none;
-			font: bold 14px monospace;
-			color: #888;
-			user-select: none;
-			touch-callout: none;
-			-webkit-user-select: none;
-			-webkit-touch-callout: none;
-		}
-		.vjoy-button:active {
-			background: #888;
-		}
+.vjoy-button {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	background: #444;
+	border: none;
+	font: bold 14px monospace;
+	color: #888;
+	user-select: none;
+	touch-callout: none;
+	-webkit-user-select: none;
+	-webkit-touch-callout: none;
+}
+.vjoy-button:active {
+	background: #888;
+}
 
-		#vjoy-button-up {
-			position: absolute;
-			left: 30%;
-			top: 0px;
-			width: 40%;
-			height: 45%;
-			border-radius: 0px 0px 50% 50%;
-		}
+#vjoy-button-up {
+	position: absolute;
+	left: 30%;
+	top: 0px;
+	width: 40%;
+	height: 45%;
+	border-radius: 0px 0px 50% 50%;
+}
 
-		#vjoy-button-down {
-			position: absolute;
-			left: 30%;
-			bottom: 0px;
-			width: 40%;
-			height: 45%;
-			border-radius: 50% 50% 0px 0px;
-		}
+#vjoy-button-down {
+	position: absolute;
+	left: 30%;
+	bottom: 0px;
+	width: 40%;
+	height: 45%;
+	border-radius: 50% 50% 0px 0px;
+}
 
-		#vjoy-button-left {
-			position: absolute;
-			left: 0px;
-			bottom: 30%;
-			width: 45%;
-			height: 40%;
-			border-radius: 0px 50% 50% 0px;
-		}
+#vjoy-button-left {
+	position: absolute;
+	left: 0px;
+	bottom: 30%;
+	width: 45%;
+	height: 40%;
+	border-radius: 0px 50% 50% 0px;
+}
 
-		#vjoy-button-right {
-			position: absolute;
-			right: 0px;
-			bottom: 30%;
-			width: 45%;
-			height: 40%;
-			border-radius: 50% 0px 0px 50%;
-		}
+#vjoy-button-right {
+	position: absolute;
+	right: 0px;
+	bottom: 30%;
+	width: 45%;
+	height: 40%;
+	border-radius: 50% 0px 0px 50%;
+}
 
-		#vjoy-button-pri {
-			position: absolute;
-			right: 0px;
-			top: 30%;
-			width: 50%;
-			height: 50%;
-			border-radius: 50%;
-		}
+#vjoy-button-pri {
+	position: absolute;
+	right: 0px;
+	top: 30%;
+	width: 50%;
+	height: 50%;
+	border-radius: 50%;
+}
 
-		#vjoy-button-sec {
-			position: absolute;
-			left: 0px;
-			top: 30%;
-			width: 50%;
-			height: 50%;
-			border-radius: 50%;
-		}
+#vjoy-button-sec {
+	position: absolute;
+	left: 0px;
+	top: 30%;
+	width: 50%;
+	height: 50%;
+	border-radius: 50%;
+}
 
-		#vjoy-button-ter {
-			position: fixed;
-			right: 0px;
-			bottom: 0px;
-			width: 10vw;
-			height: 8vmin;
-			border-radius: 8px;
-			opacity: 0.5;
-		}
+#vjoy-button-ter {
+	position: fixed;
+	right: 0px;
+	bottom: 0px;
+	width: 10vw;
+	height: 8vmin;
+	border-radius: 8px;
+	opacity: 0.5;
+}
 	`;
 
 
