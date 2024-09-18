4e79336dbb6981661c6ee608bb0ec1157f91eef2
Ben Gillbanks <binarymoon@gmail.com>
2024-09-18 10:01:41 +0100

    Improve actor animation code
    add additional actors and fix spaceship code to use new characters

--- src/internal/actors.js
+++ src/internal/actors.js
@@ -11,12 +11,12 @@
 		},
 		'move-right': {
 			frames: [ 1, 2 ],
-			fps: 2,
+			fps: 4,
 			loop: true
 		},
 		'move-left': {
 			frames: [ 1, 2 ],
-			fps: 2,
+			fps: 4,
 			loop: true,
 			direction: 1
 		},
@@ -33,7 +33,7 @@
 		},
 		'explode': {
 			frames: [ 0, 1, 2, 3 ],
-			fps: 8,
+			fps: 16,
 			loop: false
 		}
 	};
@@ -72,6 +72,54 @@
 
 
 	/**
+	 * Draw an actor at a specific position.
+	 * This ignores the text grid/ cursor position and draws at specific x, y coordinates.
+	 * This is useful for drawing actors in the game world and real-time apps.
+	 *
+	 * @param {number} ch The character to draw.
+	 * @param {string} animation The animation to draw.
+	 * @param {number} x The x coordinate to draw the actor at.
+	 * @param {number} y The y coordinate to draw the actor at.
+	 * @returns {boolean} True if the animation is still playing, false if it has finished.
+	 */
+	beep8.Actors.spr = function( ch, animation, x, y, startTime = null ) {
+
+		beep8.Utilities.checkInt( "ch", ch );
+		beep8.Utilities.checkString( "animation", animation );
+		beep8.Utilities.checkNumber( "x", x );
+		beep8.Utilities.checkNumber( "y", y );
+		if ( startTime !== null ) beep8.Utilities.checkNumber( "startTime", startTime );
+
+		const frame = animationFrame( animation, startTime );
+
+		const font = beep8.Core.textRenderer.curActors_;
+
+		const chrIndex = ( ch * font.getColCount() ) + frame;
+
+		const anim = beep8.Actors.animations[ animation ];
+
+		// Example usage:
+		if ( !shouldLoopAnimation( anim, startTime ) ) {
+			return false;
+		}
+
+		// Draw the actor.
+		beep8.Core.textRenderer.spr(
+			chrIndex,
+			x,
+			y,
+			font,
+			anim.direction || 0
+		);
+
+		// The animation is still playing.
+		return true;
+
+	}
+
+
+
+	/**
 	 * Get the current frame of an animation.
 	 * This is used internally to automatically determine what frame to draw.
 	 * This uses delta time to determine the current frame.
@@ -79,7 +127,7 @@
 	 * @param {string} animation The animation to get the frame for.
 	 * @returns {number} The frame to draw for the animation.
 	 */
-	beep8.Actors.animationFrame = function( animation, startTime = null ) {
+	const animationFrame = function( animation, startTime = null ) {
 
 		// Does the animation exist.
 		if ( beep8.Actors.animations[ animation ] === undefined ) {
@@ -106,7 +154,11 @@
 			const totalTime = beep8.Core.getNow() - startTime;
 			const frameCount = anim.frames.length;
 			const frameDuration = 1 / anim.fps;
-			const frameIndex = Math.floor( ( totalTime / 1000 ) / frameDuration % frameCount ); // Dividing by 1000 to convert ms to seconds
+
+			// Dividing totalTime by 1000 to convert ms to seconds.
+			// Dividing by frameDuration to get the current frame.
+			// Modulo frameCount to loop the animation.
+			const frameIndex = Math.floor( ( totalTime / 1000 ) / frameDuration % frameCount );
 
 			frame = anim.frames[ frameIndex ];
 
@@ -118,50 +170,27 @@
 
 
 	/**
-	 * Draw an actor at a specific position.
-	 * This ignores the text grid/ cursor position and draws at specific x, y coordinates.
-	 * This is useful for drawing actors in the game world and real-time apps.
+	 * Checks if the animation has finished looping.
 	 *
-	 * @param {number} ch The character to draw.
-	 * @param {string} animation The animation to draw.
-	 * @param {number} x The x coordinate to draw the actor at.
-	 * @param {number} y The y coordinate to draw the actor at.
-	 * @returns {boolean} True if the animation is still playing, false if it has finished.
+	 * @param {Object} anim - The animation object.
+	 * @param {number} startTime - The start time of the animation.
+	 * @returns {boolean} - Returns true if the animation should continue, false if it has finished looping.
 	 */
-	beep8.Actors.spr = function( ch, animation, x, y, startTime = null ) {
+	const shouldLoopAnimation = function( anim, startTime ) {
 
-		beep8.Utilities.checkInt( "ch", ch );
-		beep8.Utilities.checkString( "animation", animation );
-		beep8.Utilities.checkNumber( "x", x );
-		beep8.Utilities.checkNumber( "y", y );
-		if ( startTime !== null ) beep8.Utilities.checkNumber( "startTime", startTime );
-
-		const frame = beep8.Actors.animationFrame( animation, startTime );
-
-		const font = beep8.Core.textRenderer.curActors_;
-
-		const chrIndex = ( ch * font.getColCount() ) + frame;
-
-		const anim = beep8.Actors.animations[ animation ];
-
-		// Quit if the animation has finished looping.
-		if ( startTime !== null && anim.loop === false ) {
-			const animationLength = anim.frames.length * ( 1000 / anim.fps );
-			if ( beep8.Core.getNow() - startTime >= animationLength ) {
-				return false;
-			};
+		// If the animation has not started or is set to loop, continue the animation.
+		if ( startTime === null || anim.loop === true ) {
+			return true;
 		}
 
-		// Draw the actor.
-		beep8.Core.textRenderer.spr(
-			chrIndex,
-			x,
-			y,
-			font,
-			anim.direction || 0
-		);
+		// Calculate the total length of the animation in milliseconds.
+		const animationLength = anim.frames.length * ( 1000 / anim.fps );
 
-		// The animation is still playing.
+		// Check if the current time exceeds the animation length.
+		if ( beep8.Core.getNow() - startTime >= animationLength ) {
+			return false;
+		}
+
 		return true;
 
 	}
