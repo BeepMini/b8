2f519e7738969600dc064b1dab6b267bd7ba7217
Ben Gillbanks <binarymoon@gmail.com>
2025-03-11 21:30:16 +0000

    Update scene.js
    Make function names simpler and add pause and resume  methods

--- src/internal/scene.js
+++ src/internal/scene.js
@@ -5,7 +5,7 @@
 	 *
 	 * @type {Object}
 	 */
-	beep8.Scenes = {};
+	beep8.Scene = {};
 
 
 	/**
@@ -21,13 +21,29 @@
 	/**
 	 * Adds a new scene to the scene manager.
 	 *
+	 * A scene should be a javascript object with at least one of the following functions:
+	 *
+	 * - init (optional): A function that will be called when the scene is set.
+	 * - update (optional): A function that will be called multiple times a frame and passed a deltatime value as a parameter.
+	 * - render (optional): A function that will be called every frame.
+	 *
+	 * Init can be used to set up the scene. For asynchronous games you can add a while loop here and use await functions (eg for keypresses) and then render yourself.
+	 * For synchronous games you can use the update and render functions to manage game logic and rendering efficiently.
+	 *
+	 * eg:
+	 * const game = {
+	 *  init: () => { }
+	 *  update: ( dt ) => { }
+	 *  render: () => { }
+	 * }
+	 *
 	 * @param {string} name - The name of the scene.
 	 * @param {object} gameObject - An object that includes init, update, and
 	 * render methods as well as other properties for the scene. If update and
 	 * render are set then these will be passed to `beep8.frame`.
 	 * @param {number} frameRate - The frame rate at which to update and render
 	 */
-	beep8.Scenes.add = function( name, gameObject = null, frameRate = 30 ) {
+	beep8.Scene.add = function( name, gameObject = null, frameRate = 30 ) {
 
 		beep8.Utilities.checkString( 'name', name );
 
@@ -51,7 +67,7 @@
 	 *
 	 * @param {string} name - The name of the scene to switch to.
 	 */
-	beep8.Scenes.setActive = function( name ) {
+	beep8.Scene.set = function( name ) {
 
 		beep8.Utilities.checkString( 'name', name );
 
@@ -82,11 +98,51 @@
 
 
 	/**
+	 * Pauses the current scene.
+	 *
+	 * @param {string} name - The name of the scene to pause.
+	 * @returns {void}
+	 */
+	beep8.Scene.pause = function() {
+
+		beep8.frame( null );
+
+	};
+
+
+	/**
+	 * Resumes the current scene.
+	 *
+	 * @returns {void}
+	 */
+	beep8.Scene.resume = function() {
+
+		// If there's no active scene, do nothing.
+		if ( !activeScene ) {
+			return;
+		}
+
+		// Get the currentScene.
+		const currentScene = sceneList[ activeScene ];
+
+		// If there's an update or render method, call frame to create a synchronous game.
+		if ( currentScene.update || currentScene.render ) {
+			beep8.frame(
+				currentScene.render || ( () => { } ),
+				currentScene.update || ( () => { } ),
+				currentScene.frameRate || 30
+			);
+		}
+
+	};
+
+
+	/**
 	 * Gets the current active scene.
 	 *
 	 * @returns {Object|null} The active scene object, or null if no scene is active.
 	 */
-	beep8.Scenes.getActive = function() {
+	beep8.Scene.get = function() {
 
 		return activeScene;
 
@@ -98,7 +154,7 @@
 	 *
 	 * @returns {Object} All scenes.
 	 */
-	beep8.Scenes.getAll = function() {
+	beep8.Scene.getAll = function() {
 
 		return sceneList;
 
