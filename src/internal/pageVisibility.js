4ec8958c88aa65871d204ef8899a3595512e74ac
Ben Gillbanks <binarymoon@gmail.com>
2025-05-27 21:59:18 +0100

    Create pageVisibility.js
    Add code that automatically tracks if the page is visible or not and fires events that can be listened to. This is used in the music module to stop music from playing when the game is no longer in focus

---
+++ src/internal/pageVisibility.js
@@ -0,0 +1,65 @@
+( function( beep8 ) {
+
+	let timeHidden = 0;
+	let isHidden = false;
+
+
+	/**
+	 * Called when the document is hidden (blurred).
+	 *
+	 * @returns {void}
+	 */
+	function sleep() {
+
+		if ( isHidden ) return;
+
+		isHidden = true;
+
+		// Document is hidden (blurred) so start the timer.
+		timeHidden = Date.now();
+
+		// Log the event and notify listeners.
+		beep8.Utilities.event( 'pageVisibility.sleep' );
+
+	};
+
+
+	/**
+	 * Called when the document is shown (focused).
+	 *
+	 * @returns {void}
+	 */
+	function wake() {
+
+		if ( !isHidden ) return;
+		isHidden = false;
+
+		if ( timeHidden === 0 ) return;
+
+		// Document is shown again so the timer is stopped and totalled.
+		const timeAsleep = Date.now() - timeHidden;
+		beep8.Utilities.log( 'Time asleep:', ( timeAsleep / 1000 ).toFixed( 3 ) );
+		beep8.Utilities.event( 'pageVisibility.wake', { time: timeAsleep } );
+		timeHidden = 0;
+
+	}
+
+
+	// Set an event when the document loses focus (change tab/ window).
+	document.addEventListener(
+		'visibilitychange',
+		function() {
+
+			if ( document.hidden ) {
+				sleep();
+			} else {
+				wake();
+			}
+
+		}
+	);
+
+	window.addEventListener( 'blur', () => sleep() );
+	window.addEventListener( 'focus', () => wake() );
+
+} )( beep8 || ( beep8 = {} ) );
