ba9e23a1e75f4f9b88324f7a33db93a1122f49e0
Ben Gillbanks <binarymoon@gmail.com>
2025-08-28 22:44:11 +0100

    Simplify cursor rendering logic and config
    Removed unused cursor width, height, and offset config options. Refactored cursor rendering to use font metrics directly and simplified the draw function signature, improving maintainability and consistency.

--- src/internal/cursorrenderer.js
+++ src/internal/cursorrenderer.js
@@ -44,42 +44,32 @@
 
 
 	/**
-	 * Draws the cursor.
+	 * Draws the flashing cursor.
+	 * This is called automatically in the beep8 render function so does not
+	 * need to be called manually.
 	 *
 	 * @param {CanvasRenderingContext2D} targetCtx - The context to draw the cursor on
-	 * @param {number} canvasWidth - The width of the canvas
-	 * @param {number} canvasHeight - The height of the canvas
 	 * @returns {void}
 	 */
-	beep8.CursorRenderer.draw = function( targetCtx, canvasWidth, canvasHeight ) {
+	beep8.CursorRenderer.draw = function( targetCtx ) {
 
 		beep8.Utilities.checkInstanceOf( "targetCtx", targetCtx, CanvasRenderingContext2D );
-		beep8.Utilities.checkNumber( "canvasWidth", canvasWidth );
-		beep8.Utilities.checkNumber( "canvasHeight", canvasHeight );
 
 		// If the cursor is not visible or it is not time to blink, do nothing.
 		if ( !beep8.Core.drawState.cursorVisible || blinkCycle_ <= 0 ) return;
 
-		const ratio = canvasWidth / beep8.Core.canvas.width;
+		const font = beep8.TextRenderer.getFont();
 
 		// Calculate the real position of the cursor.
-		const realX = Math.round(
-			( beep8.Core.drawState.cursorCol + 0.5 - beep8.CONFIG.CURSOR.WIDTH_F / 2 + beep8.CONFIG.CURSOR.OFFSET_H ) *
-			beep8.CONFIG.CHR_WIDTH * ratio
-		);
-
-		const realY = Math.round(
-			( beep8.Core.drawState.cursorRow + 1 - beep8.CONFIG.CURSOR.HEIGHT_F - beep8.CONFIG.CURSOR.OFFSET_V ) *
-			beep8.CONFIG.CHR_HEIGHT * ratio
-		);
+		const realX = beep8.Core.drawState.cursorCol * beep8.CONFIG.CHR_WIDTH;
+		const realY = beep8.Core.drawState.cursorRow * beep8.CONFIG.CHR_HEIGHT;
 
 		// Draw the cursor.
 		targetCtx.fillStyle = beep8.Core.getColorHex( beep8.Core.drawState.fgColor );
 
 		targetCtx.fillRect(
-			realX, realY,
-			Math.round( beep8.CONFIG.CURSOR.WIDTH_F * beep8.CONFIG.CHR_WIDTH * ratio ),
-			Math.round( beep8.CONFIG.CURSOR.HEIGHT_F * beep8.CONFIG.CHR_HEIGHT * ratio )
+			realX + 1, realY + 1,
+			font.charWidth_ - 2, font.charHeight_ - 2
 		);
 
 	}
