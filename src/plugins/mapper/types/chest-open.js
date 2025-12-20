f7069e5933a6c7b4ad006b9df748ecb56a25623b
Ben Gillbanks <binarymoon@gmail.com>
2025-12-20 22:47:30 +0000

    Refactor chestOpen spawn to conditionally add message
    Updated the chestOpen spawn function to only add Message and Action components if a message is provided. Also changed the Message property from 'message' to 'text' for consistency.

--- src/plugins/mapper/types/chest-open.js
+++ src/plugins/mapper/types/chest-open.js
@@ -2,23 +2,24 @@ mapper.types.chestOpen = {
 
 	spawn: function( col, row, props ) {
 
-		return b8.ECS.create(
-			{
-				Type: { name: 'chest' },
-				Loc: { col, row },
-				Sprite: {
-					tile: 271,
-					fg: props.fg || 15,
-					bg: props.bg || 0,
-					depth: 10
-				},
-				Solid: {},
-				Message: { message: props.message || "" },
-				Action: {
-					verb: 'read'
-				},
-			}
-		);
+		const entitySettings = {
+			Type: { name: 'chest' },
+			Loc: { col, row },
+			Sprite: {
+				tile: 271,
+				fg: props.fg || 15,
+				bg: props.bg || 0,
+				depth: 10
+			},
+			Solid: {},
+		};
+
+		if ( props.message ) {
+			entitySettings.Message = { text: props.message };
+			entitySettings.Action = { verb: 'read' };
+		}
+
+		return b8.ECS.create( entitySettings );
 
 	},
 
